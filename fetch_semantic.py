ac48bdafa9e4d42abce92c3cecc7587eafdf0c44
Fahim <49230752+evilgodfahim@users.noreply.github.com>
2025-12-17 20:09:51 +0600

    Update fetch_semantic.py

--- fetch_semantic.py
+++ fetch_semantic.py
@@ -16,7 +16,9 @@ FEEDS = [
     "https://www.dhakatribune.com/feed/",
     "https://evilgodfahim.github.io/ds/todays_news.xml",
 "https://evilgodfahim.github.io/gd/merged.xml",
-"https://evilgodfahim.github.io/bangladesh/feed.xml"
+"https://evilgodfahim.github.io/bangladesh/feed.xml",
+"https://evilgodfahim.github.io/sci/feed_master.xml",
+"https://evilgodfahim.github.io/climate/combined.xml"
 ]
 
 RESULT_XML = "result.xml"

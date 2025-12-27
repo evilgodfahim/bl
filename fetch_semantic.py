996c1ba16495938d84ad428fcd21886c651581b6
Fahim <49230752+evilgodfahim@users.noreply.github.com>
2025-12-27 17:16:39 +0600

    Update fetch_semantic.py

--- fetch_semantic.py
+++ fetch_semantic.py
@@ -28,7 +28,7 @@ CACHE_JSON = "cache.json"
 MAX_TOTAL = 1000
 MAX_PER_FEED = 50
 MAX_EXIST = 50
-SIM_THRESH = 0.50
+SIM_THRESH = 0.65
 BLOCK_PARTS = ("/opinion/", "/editorial/", "/sports/", "/sport/", "/entertainment/", "/showtime/", "/video/", "/business/")
 IMG_RE = re.compile(r'<img[^>]+src="([^"]+)"')
 DAYS_TO_KEEP = 7
